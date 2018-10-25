01b476c025ef56f9036aac69d1efb91c7202c104
BT_1N3_15 <jfreund5400@bths.edu>
2018-10-25 14:42:07 -0400

    edit

--- src/Student.java
+++ src/Student.java
@@ -1,11 +1,21 @@
 public class Student extends Person {
     private double GPA;
-    public Student(double GPA){
+    private int age;
+    private int grade;
+    public Student(double GPA, int age, int grade){
         super("","");
         this.GPA= GPA;
+        this.age= age;
+        this.grade= grade;
     }
     public double getGPA(){
         return GPA;
+    }
+    public int getage(){
+        return age;
+    }
+    public int getgrade(){
+        return grade;
     }
     public String toString(){
         return getFamilyName()+","+getFirstName();
