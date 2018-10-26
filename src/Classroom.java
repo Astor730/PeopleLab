202b7f69542434cec89fc22a90ecaac64de1c728
BT_1N3_14 <rstraight8821@bths.edu>
2018-10-26 14:59:05 -0400

    We did it!

--- src/Classroom.java
+++ src/Classroom.java
@@ -3,23 +3,24 @@ public class Classroom extends Teacher
 
     private Teacher teacher;
     private Student[]students;
-    public Classroom (teacher, students)
+    public Classroom (Teacher teacher, Student[] students)
     {
         super("Math", "Mr.");
         this.teacher=teacher;
         this.students=students;
-
-
-
-        for(students : Student)
-        {
-            getGPA();
-            System.out.println(gpa+GPA);
-
-        }
-
-
-
     }
+    double average = 0;
+    public double classAverage()
+    {
+        double x = 0;
+        for (int i = 0; i<students.length; i++)
+        {
+            x = x+ students[i].getGPA();
+        }
+        return x/students.length;
+    }
+
+
+    public String getSubject() {return teacher.getSubject();}
 
 }
